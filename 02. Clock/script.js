8ca25473c933745c38fffbf9acca02494b2fd265
iwannabekoshka <pokusaeff.daniil@gmail.com>
2022-04-18 15:35:44 +0300

    feat[02]: 02. Clock

---
+++ 02. Clock/script.js
@@ -0,0 +1,32 @@
+const clockSeconds = document.querySelector('#clock__seconds');
+const clockMinutes = document.querySelector('#clock__minutes');
+const clockHours = document.querySelector('#clock__hours');
+
+const rotateSeconds = (seconds) => {
+  const angle = 360/60 * seconds;
+
+  clockSeconds.style.cssText = `--angle: ${angle}deg`;
+}
+const rotateMinutes = (minutes) => {
+  const angle = 360/60 * minutes;
+
+  clockMinutes.style.cssText = `--angle: ${angle}deg`;
+}
+const rotateHours = (hours) => {
+  const angle = 360/24 * hours;
+
+  clockHours.style.cssText = `--angle: ${angle}deg`;
+}
+
+setInterval(() => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  rotateSeconds(seconds);
+  rotateMinutes(minutes);
+  rotateHours(hours);
+
+  console.log(hours, minutes, seconds)
+}, 1000)
